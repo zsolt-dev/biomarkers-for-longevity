ed14fea95cc687db00ec17a64b5866a0f7e60e85
Zsolt Szabo <zsolt.dev@icloud.com>
2023-12-13 12:27:13 +0100

    add check to for biomarkers that were not imported

--- scripts/validateBiomarkers.ts
+++ scripts/validateBiomarkers.ts
@@ -1,15 +1,46 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 import { biomarkersDictionary } from '../src/biomarkers/index.js';
 import biomarkerSchema from '../src/schema/biomarker.js';
 
 let exitCode = 0;
+
+// eslint-disable-next-line no-console -- CLI
+console.log(`\n\nValidate all imported biomarkers against zod schema:\n`);
 Object.entries(biomarkersDictionary).forEach(([shortCode, biomarker]) => {
   try {
     // eslint-disable-next-line no-console -- CLI
-    console.log(`Validating: ${shortCode}`);
+    console.log(`  - valid: ${shortCode}`);
     biomarkerSchema.parse(biomarker);
   } catch (e) {
     exitCode = 1;
     console.error(`Error in biomarker: ${shortCode}`, e);
+  }
+});
+
+// eslint-disable-next-line no-console -- CLI
+console.log(`\n\nChecking if all biomarkers are imported:\n`);
+
+// get list of .json files in the biomarkers folder
+
+const currentFilePath = fileURLToPath(import.meta.url);
+const currentDir = path.dirname(currentFilePath);
+const rootDir = path.resolve(currentDir, '../');
+
+const biomarkersFolder = path.join(rootDir, 'src/biomarkers');
+const files = fs.readdirSync(biomarkersFolder);
+const jsonFiles = files.filter((file) => file.endsWith('.json'));
+jsonFiles.forEach((file) => {
+  // strip .json extension
+  const shortCode = file.slice(0, -5);
+  if (!biomarkersDictionary[shortCode]) {
+    exitCode = 1;
+    console.error(`  - NOT IMPORTED - ${shortCode}`);
+  } else {
+    // eslint-disable-next-line no-console -- CLI
+    console.log(`  - imported: ${shortCode}`);
   }
 });
 
